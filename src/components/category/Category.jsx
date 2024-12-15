02acb8806b081f05d34401ee8b9903723c229c5f
Ardy1an <adithardiyanto0@gmail.com>
2024-12-15 16:16:52 +0700

    carousel slider with swiper and tailwind

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -3,6 +3,16 @@ import "./category.css";
 import Slider from "react-perfect-slider";
 import sofa from "../../assets/sofa.png";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/free-mode";
+
+import { FreeMode, Pagination } from "swiper/modules";
+import { RxArrowTopRight } from "react-icons/rx";
+import { ServiceData } from "../../constants";
+
 const Category = ({ onClick }) => {
   return (
     <div className="category-left">
@@ -15,18 +25,44 @@ const Category = ({ onClick }) => {
         </span>
       </div>
 
-      <div className="carousel-slider1">
-        <Slider renderControls={(next, previous) => [<button onClick={previous}>Previous</button>, <button onClick={next}>Next</button>]}>
-          <div>
-            <img src={sofa} alt="sofa" className="sofa" />
-          </div>
-          <div>
-            <img src={sofa} alt="sofa" className="sofa" />
-          </div>
-          <div>
-            <img src={sofa} alt="sofa" className="sofa" />
-          </div>
-        </Slider>
+      <div className="swiper-container">
+        <Swiper
+          breakpoints={{
+            340: {
+              slidesPerView: 2,
+              spaceBetween: 15,
+            },
+            700: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+          }}
+          freeMode={true}
+          //pagination digunakan untuk menampilkan navigasi titik dibawah carousel slidernya
+          // pagination={{
+          //   clickable: true,
+          // }}
+          modules={[FreeMode]}
+          // modules={[FreeMode, Pagination]} //pagination digunakan untuk menampilkan navigasi titik dibawah carousel slidernya
+        >
+          {ServiceData.map((item) => (
+            <SwiperSlide key={item.title}>
+              {/* <SwiperSlide key={item.title} style={{ border: "1px solid red" }}> */}
+
+              <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[500px] lg:w-[250px] overflow-hidden cursor-pointer">
+                <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${item.backgroundImage})` }} />
+                <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50" />
+                <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 text-center z-10">
+                {/* <div className="relative flex flex-col gap-3"> */}
+                  {/* <item.icon className="text-blue-600 group-hover:text-blue-400 w-[32px] h-[32px]" /> */}
+                  <h1 className="text-xl lg:text-2xl">{item.title} </h1>
+                  {/* <p className="lg:text-[18px]">{item.content} </p> */}
+                </div>
+                {/* <RxArrowTopRight className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-blue-500 group-hover:rotate-45 duration-100" /> */}
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );

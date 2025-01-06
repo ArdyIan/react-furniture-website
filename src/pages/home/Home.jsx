0685d11c127a4e9bd2e18e30e900d6664c73de98
Ardy1an <adithardiyanto0@gmail.com>
2025-01-06 22:32:38 +0700

    home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,24 @@
-import Navbar from "../..components/navbar/Navbar";
+import Hero from "../../components/hero/Hero";
+import Navbar from "../../components/navbar/Navbar";
+import Category from "../../components/category/Category";
+import Header from "../../components/header/Header";
+import Advertisement from "../../components/advertisement/Advertisement";
+import DisplayProduct from "../../components/displayProduct/DisplayProduct";
+import Testimonial from "../../components/testimonial/Testimonial";
+import Subscription from "../../components/Subscription/Subscription";
 import "./home.css";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Category />
+      <Advertisement />
+      <DisplayProduct />
+      <Testimonial />
+      <Subscription />
+      {/* <Header /> */}
     </div>
   );
 };

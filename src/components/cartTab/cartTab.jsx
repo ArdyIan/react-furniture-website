a1cdb3a7d0df27d0d8369e8165958ca43ac92374
Ardy1an <adithardiyanto0@gmail.com>
2025-02-06 13:38:47 +0700

    cart tab

--- src/components/cartTab/cartTab.jsx
+++ src/components/cartTab/cartTab.jsx
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import CartItem from "../cartItem/cartItem";
 import { toggleStatusTab } from "../../stores/cart";
 import React, { useEffect } from "react";
-
+import "./carttab.css";
 const CartTab = () => {
   const carts = useSelector((store) => store.cart.items);
   const statusTab = useSelector((store) => store.cart.statusTab);
@@ -21,25 +21,21 @@ const CartTab = () => {
   };
 
   return (
-    
-    <div className={`fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr_60px] transform transition-transform duration-500 ${statusTab ? "translate-x-0" : "translate-x-full"}`}>
-      {/* <h2 className="text-white">CartTab Debugging</h2> */}
-      <h2 className="p-5 text-white text-2xl">Shopping Cart</h2>
-      <div className="p-5 flex-1 overflow-y-auto">
+    <div className={`cart-tab-12 ${statusTab ? "opened-cart" : "closed-cart"}`}>
+      <h2 className="cart-header-tab">Shopping Cart</h2>
+      <div className="cart-content-tab">
         {carts.map((item, key) => (
           <CartItem key={key} data={item} />
         ))}
       </div>
-      <div className="grid grid-cols-2">
-        <button className="bg-black text-white" onClick={handleCloseTab}>
+      <div className="cart-footer-tab">
+        <button className="btn-closed-tab" onClick={handleCloseTab}>
           CLOSE
         </button>
-        <button className="bg-amber-600 text-white">Checkout</button>
+        <button className="btn-checkout-tab">Checkout</button>
       </div>
-      {/* <div>cart tab testing</div> */}
     </div>
   );
-
 };
 
 export default CartTab;

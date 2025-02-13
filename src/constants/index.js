78231e2fc9b34f8d6268e731a5c2910ad039796e
Ardy1an <adithardiyanto0@gmail.com>
2025-02-13 20:22:56 +0700

    image assets json

---
+++ src/constants/index.js
@@ -0,0 +1,55 @@
+import {
+    RxCrop,
+    RxDesktop,
+    RxPencil2,
+    RxReader,
+    RxRocket,
+    RxAccessibility,
+  } from "react-icons/rx";
+  
+  import table from "../assets/table.jpg";
+  import chair from "../assets/chair.png";
+  import bed from "../assets/bed.png";
+  import cupboard from "../assets/cupboard.png";
+  import lighting from "../assets/lighting.png";
+  import art from "../assets/art-display.jpg";
+
+  
+  export const ServiceData = [
+    {
+      icon: RxCrop,
+      title: "Table",
+      content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
+      backgroundImage: table,
+    },
+    {
+      icon: RxPencil2,
+      title: "Chair",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      backgroundImage: chair,
+    },
+    {
+      icon: RxDesktop,
+      title: "Bed",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      backgroundImage: bed,
+    },
+    {
+      icon: RxReader,
+      title: "Cupboard",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      backgroundImage: cupboard,
+    },
+    {
+      icon: RxAccessibility,
+      title: "Lighting",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      backgroundImage: lighting,
+    },
+    {
+      icon: RxRocket,
+      title: "Art Display",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      backgroundImage: art,
+    },
+  ];
